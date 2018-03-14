23c48ba7cb7a6f236dbb926e85c302fa10345aa7
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-14 17:18:19 +0000

    button printing in console

--- cityquiz-swift/ViewController.swift
+++ cityquiz-swift/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+    print("Hello World!")
+    }
+    
 }
 
