9544fdc932b47bbc0a2d541d7fbf9522b7c0bcc3
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-14 17:02:26 +0000

    Initial Commit

---
+++ cityquiz-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cityquiz-swift
+//
+//  Created by Amadea Kimmins on 14/03/2018.
+//  Copyright © 2018 Amadea Kimmins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
